666a64f1dd45e31521335c8ba982ab3a028d4970
recardomu-eng <recardomu@gmail.com>
2025-12-23 11:38:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import streamlit as st
 
 st.title("Калькулятор процентов 🧮")
-
+operation = st.selectbox(
+    "Выберите операцию:",
+    ["Найти % от числа", "Прибавить % к числу", "Вычесть % из числа"]
+)
 number = st.number_input("Введите число", value=0.0)
 percent = st.number_input("Введите процент", value=0.0)
 
 if st.button("Рассчитать"):
-    result = (number * percent) / 100
-
-    st.success(f"Результат: {percent}% от {number} равно {result}")
+   if operation == "Найти % от числа":
+        result = (number * percent) / 100
+        st.success(f"Результат: {result}")
+    elif operation == "Прибавить % к числу":
+        result = number + (number * percent) / 100
+        st.success(f"Итоговая сумма: {result}")
+    elif operation == "Вычесть % из числа":
+        result = number - (number * percent) / 100
+        st.success(f"Цена со скидкой: {result}")
+        
 st.sidebar.header("О проекте ℹ️")
 st.sidebar.write("Этот калькулятор создал In$Aide$4.")
+
 
 
